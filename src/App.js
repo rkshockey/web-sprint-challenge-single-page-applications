49b94d18055ab50177ab6409cee343413386b559
rkshockey <rkshockey@gmail.com>
2021-07-23 12:36:31 -0400

    Added pizza route and button and created pizza form skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route, Switch, Link } from 'react-router-dom';
+import PizzaForm from './components/PizzaForm';
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className='page'>
+      <Switch>
+        <Route exact path='/'>
+          <h1>Lambda Eats</h1>
+          <p>You can remove this code and create your own header</p>
+          <Link to='/pizza'>
+            <button id='order-pizza'>Pizza?</button>
+          </Link>
+        </Route>
+        <Route path='/pizza'>
+          <PizzaForm />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PizzaForm (props) {
+    return null
+}
+
+export default PizzaForm
